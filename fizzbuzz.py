ba773009f8c1eceb0ea0b700bc58d3f09a68c2a6
TJ-Ahn <atj528@naver.com>
2023-02-03 14:24:54 +0900

    feat: Add if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,9 @@
 user_num = int(input('Hello, there! Enter some positive integer: '))
 
-# Print user_num for debugging
-print(user_num)
 
 # Iteration
 for i in range(1, user_num+1):
-    print(f'{i}')
+    if i%3==0:
+        print('fizz')
+    else:
+        print(f'{i}')
